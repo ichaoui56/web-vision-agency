7a4af53b984d706fe7eb2e3bf1a72aeff2768f28
CnomDev2 <dev2@cnomweb.com>
2025-09-07 17:06:54 +0100

    add margin to bottom

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -292,7 +292,7 @@ const MyPage = () => {
             </div>
             <div className="section">
               <div className="relative md:h-screen w-screen gap-4 p-10 flex justify-center items-center flex-col overflow-hidden">
-                <div className="z-0 mb-48 md:mb-0 md:absolute top-1/4 md:right-[10%] md:-translate-y-16">
+                <div className="z-0 mb-[196px] md:mb-0 md:absolute top-1/4 md:right-[10%] md:-translate-y-16">
                   <motion.div
                     className="bg-slate-300 rounded-sm h-[400px] md:h-[600px] w-[80vw] md:w-[30vw] grayscale hover:grayscale-0"
                     initial={{
